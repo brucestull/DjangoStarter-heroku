9a8d72c76a1f578dadf4077025e50187b44ef539
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-06-09 19:20:58 -0400

    Edited model comments.

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ class CustomUser(AbstractUser):
     """
     A CustomUser class is added so we can add functionality later. It's more convenient then not to add CustomUser at beginning of project before database migrations are started.
     """
+    # Add additional fields in here and remove `pass` line below.
     pass
-    # add additional fields in here
 
     def __str__(self):
         """
