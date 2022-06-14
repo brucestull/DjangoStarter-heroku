35b935c9a27472ffe7db2a7b2bbddbbec3e0f14a
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-06-14 11:18:30 -0400

    Added 'localhost' to 'ALLOWED_HOSTS'.

--- my_current_project/settings/common.py
+++ my_current_project/settings/common.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
