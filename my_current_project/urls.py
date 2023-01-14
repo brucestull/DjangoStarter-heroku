4f6f8a09963a792162ca74c959531a6e86b13826
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-01-14 06:59:28 -0500

    Separate 'staff' and 'user' links into tamplates which can be 'include'd in other tamplates.

--- my_current_project/urls.py
+++ my_current_project/urls.py
@@ -20,8 +20,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
+
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+    
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
