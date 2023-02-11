bca975085d646b9127be6b16ddd392ef6ba67031
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-02-11 04:59:25 -0500

    Remove 'Pipfile' from 'notes' directory, remove comment from 'config/settings/common.py'.

--- config/settings/common.py
+++ config/settings/common.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates'],  # Our change
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
