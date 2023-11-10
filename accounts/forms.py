e2eb56c6c944fbb866a63919e7b31357baaf0042
FlynntKnapp <bruce.stull@gmail.com>
2023-11-10 16:44:17 -0500

    Fix flake8 errors in 'accounts/forms.py'.

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,31 +2,29 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from accounts.models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
 
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = (
-            'username',
-            'email',
+            "username",
+            "email",
             # 'first_name',
             # 'last_name',
         )
 
-class CustomUserChangeForm(UserChangeForm):
 
+class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = (
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-
+            "username",
+            "email",
+            "first_name",
+            "last_name",
             # 'is_staff',   # This field should only be available to
             # some adminstrative role. There would be some sort of page
             # where the andminstrative role can change the role of a user.
-
             # 'is_superuser',   # This field should only be available to
             # some adminstrative role. There would be some sort of page
             # where the andminstrative role can change the role of a user.
