4ecfac488d656acb6bb4bf210a1863c6d18e0d29
FlynntKnapp <bruce.stull@gmail.com>
2023-11-12 05:57:03 -0500

    Remove import of 'os'.

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,1 @@
-import os
-
 from config.settings.common import *  # noqa: F403, F401
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,3 @@
-import os
-
 from config.settings.common import *  # noqa: F403, F401
 
 MIDDLEWARE = MIDDLEWARE + ["whitenoise.middleware.WhiteNoiseMiddleware"]  # noqa: F405
